6f874b624efd09ad845ea6353f6d7afff8ec6b0b
joaquinrey07 <joaquinperez@liceovvh.cl>
2026-04-20 10:27:59 -0400

    core

---
+++ python/fundamentos/bucles_for_basico_1_(Core)_joaquinperez_14-04-2026/bucle_for_basico1.py
@@ -0,0 +1,60 @@
+"""
+En este archivo pondrás en práctica el uso de bucles 'for' en Python,
+usando ejemplos inspirados en videojuegos y situaciones atractivas.
+"""
+
+# 1. Generador de niveles
+# Imprime todos los niveles del 0 al 100 (incluyendo el 100).
+
+# (for i in range(1, 101):
+# print(i))
+
+
+# 2. Potenciadores de energía (Múltiplos de 2)
+# Imprime los números múltiplos de 2 desde 2 hasta 500 (incluyendo el 500).
+
+# (for i in range(2, 501, 2):
+# print(i))
+
+
+# 3. Trampa de emojis
+# Recorre los puntos del 1 al 100.
+# - Si el número es divisible por 5, imprime ""
+# - Si es divisible por 10, imprime ""
+# ¡Cuidado con la prioridad en tus condicionales!
+# (for i in range(1, 101):
+# if i % 10 == 0:
+#        print("trampa")
+#    elif i % 5 == 0:
+#        print("sorpresa")
+#    else:
+#        print(i))
+
+
+
+# 4. Suma colosal
+# Suma todos los números pares del 0 al 500,000 e imprime la suma total.
+# (suma = 0
+# for numero in range(0, 500001, 2):
+#    suma += numero
+# print(f"Suma total números pares: {suma}"))
+
+
+# 5. Retroceso temporal
+# Desde 2024, retrocede de 3 en 3 hasta 0 o menos.
+# Imprime cada valor en la cuenta regresiva.
+# (for año in range(2024, -1, -3):
+#    print(f"Año: {año}"))
+
+
+# 6. Contador dinámico
+# Declara las variables inicio, fin, y salto (por ejemplo: inicio=3, fin=10, salto=2).
+# Imprime los números en el rango que sean múltiplos de 'salto'.
+# (inicio = 3
+# fin = 10
+# salto = 2
+# for i in range(inicio + 1, fin + 1, salto):
+#    print(i))
+
+# Ejemplo: si inicio = 3, fin = 10, y salto = 2
+# Se imprimiría: 4, 6, 8, 10
